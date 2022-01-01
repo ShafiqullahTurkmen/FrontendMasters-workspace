b9679e6d7baa196dde33ebd5bd326b354dc5c861
shafiqullah turkmen <shafiqullahturkmen@gmail.com>
2022-01-01 22:30:35 +0300

    Non-valid Characters

--- JavaScript/01.from-fundamentals-to-functional-JS/01-Objects.js
+++ JavaScript/01.from-fundamentals-to-functional-JS/01-Objects.js
@@ -7,7 +7,7 @@ var who = person.name;
 
 person[0] = "I was not in the Billiards room";
 
-console.log(person[0], person["name"]);
+console.log(person);
 
 console.log("***************************************************");
 
@@ -26,3 +26,38 @@ person["plea"] = "I think so";
 console.log(person.plea);
 
 console.log("***************************************************");
+
+// Non-valid Characters
+
+var box = {};
+
+box["material"] = "cardboard";
+box[0] = "meow";
+box["^&*"] = "testing 123";
+
+var test = box["^&*"];
+
+console.log(box);
+
+console.log("***************************************************");
+
+// Game Characters Challenge
+
+// Create an object using bracket and dot notation that represents the characters and 
+// related data you may find in a game of Clue
+
+var game = {};
+
+game.murderer = "??";
+game["weapons"] = [
+  { type:"laser", location: "lab"},
+  "angry cats",
+  "dish soap",
+]
+
+game.name = [];
+game.name[0] = "Miss Scarlet";
+game.name.push('Mr. Green');
+
+console.log(game);
+

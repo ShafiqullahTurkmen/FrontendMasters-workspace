983f81ae7079c37e50374bbd29982f874a43197f
shafiqullah turkmen <shafiqullahturkmen@gmail.com>
2022-01-06 18:00:37 +0300

    portfolio added

---
+++ JavaScript/01.from-fundamentals-to-functional-JS/05-filter.js
@@ -0,0 +1,24 @@
+const _ = {};
+
+_.filter = function (arr, cb) {
+  const storage = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (cb(arr[i], i, arr) === true) {
+      storage.push(arr[i]);
+    }
+  }
+  return storage;
+};
+
+
+_.filter = function (arr, cb) {
+  const storage = [];
+
+  _.each(arr, function (item, i, list) {
+    if (cb(item, i, list)) storage.push(item);
+    
+    
+  });
+
+  return storage;
+};
